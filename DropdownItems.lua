f5cef42ef5699b6086dd5b483edb8de3eb220d99
Christoffer Andersen <Christoffer>
2026-08-08 17:36:49 +0200

    Initial commit

---
+++ DropdownItems.lua
@@ -0,0 +1,16 @@
+-- 45 per menu max
+TwitchEmotes_dropdown_options = {
+	[1] = {
+		"lizzievangy", "lizzievarrive", "lizzievblankies", "lizzievblush", "lizzievbongo", "lizzievbubblegum",
+		"lizzievcat", "lizzievcheer", "lizzievcheers", "lizzievchug", "lizzievclap", "lizzievclean",
+		"lizzievcoffee", "lizzievcopium", "lizzievcozy", "lizzievcult", "lizzievdetective", "lizzievdinkdonk",
+		"lizzievfight", "lizzievfine", "lizzievflex", "lizzievflower", "lizzievfy", "lizzievgiggle",
+		"lizzievgun", "lizzievheadbang", "lizzievhearteyes", "lizzievhi", "lizzievhide", "lizzievhowdy",
+		"lizzievhug", "lizzievkiss", "lizzievl", "lizzievlate", "lizzievlegs", "lizzievlick",
+		"lizzievlove", "lizzievnou", "lizzievsad", "lizzievshy", "lizzievsip", "lizzievslay",
+		"lizzievsmush", "lizzievstab", "lizzievtoffla"
+	},
+	[2] = {
+		"lizzievuwu", "lizzievwiggle", "lizzievwowgi", "lizzievyessir"
+	}
+};
